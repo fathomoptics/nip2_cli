337921bcfadf9c2867c43649f41468cab570256f
Matt Hirsch <matt@lumiidisplay.com>
2020-02-14 00:44:52 -0500

    macroworker generates barriers from url

---
+++ index.js
@@ -0,0 +1,27 @@
+/* jslint esversion: 8 */
+const util = require('util');
+const process = require('process');
+const path = require('path');
+const execFile = util.promisify(require('child_process').execFile);
+
+const options = {
+  tabName: 'tab1',
+  sheetName: 'workspace.ws',
+  sheetPath: process.cwd()
+};
+
+module.exports.setOptions = function(o) {
+  Object.assign(options, o);
+};
+
+module.exports.nip2 = async function(main, cellMap, outPath) {
+  var commandArgs = Object.keys(cellMap).reduce( (acc, ele) => acc.concat(['-=',  expandName(ele) + '=' + args[ele] ]), []);
+  commandArgs.unshift('-bp');
+  commandArgs = commandArgs.concat(['-=', 'main=' + expandName(main), '-o', outPath, path.join(options.sheetPath, options.sheetName)]);
+  const {stdout, stderr} = await execFile('nip2', commandArgs);
+  return stdout;
+};
+
+function expandName(n) {
+  return ['Workspaces', options.tabName, n].join('.');
+}
